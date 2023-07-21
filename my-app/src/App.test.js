fa13753a12dd035362cf31384c307081020c87f3
Lily <tdong4@uw.edu>
2023-07-21 10:29:43 +0800

    2 tests added

--- my-app/src/App.test.js
+++ my-app/src/App.test.js
@@ -6,3 +6,18 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('Class test: Checking checkbox functionality', () => {
+  render(<App />);
+
+  // Event: Click on the checkbox
+  userEvent.click(screen.getByTestID("checkbox"));
+  // Checking if checkbox is not checked.
+  expect(screen.getByTestID("checkbox")).toBeChecked()
+
+});
+
+test("My test: Test to see if there is a button", () => {
+  render(<App />);
+  expect(screen.getByRole('button')).toBeInTheDocument();
+})
